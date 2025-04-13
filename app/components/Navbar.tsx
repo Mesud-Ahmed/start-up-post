dbebcac79481e8092e8fc173013c4583a884fe4c
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-04-13 21:38:25 +0300

    add logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { auth, signIn, signOut } from "@/auth";
 import Link from "next/link";
-
+import Image from "next/image";
 const Navbar = async () => {
   const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center ">
+      <Image src='/logo.png' width={80} height={80} alt="logo"></Image>
+
         <div className="flex items-center gap-5 text-black">
           {session && session.user ? (
             <>
